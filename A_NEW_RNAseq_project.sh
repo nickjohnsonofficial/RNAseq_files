461bf186979d08d5ebefc50f350f8c7ed9a69e34
Nick Johnson <105008839+nickjohnsonofficial@users.noreply.github.com>
2025-03-19 14:00:49 -0400

    Update A_NEW_RNAseq_project.sh
    fixed defining base_dir

--- A_NEW_RNAseq_project.sh
+++ A_NEW_RNAseq_project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ### IMPORTANT: Define your base directory before you run this script! ###
-base_dir=path/to/chosen/directory/
+base_dir="path/to/chosen/directory/"
 mkdir -p base_dir/RNAseq/0_zips RNAseq/1_raw_data RNAseq/2_aligned RNAseq/3_counts RNAseq/4_results RNAseq/5_logs
 cp -r base_dir/Genome_Indices/GRCm39/ base_dir/RNAseq/6_indices
 bash base_dir/scripts/B_RNAseq_analysis_all.sh
