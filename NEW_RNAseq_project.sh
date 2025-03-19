32921ae64671afaf8a1504ed18a2eb9725af9799
Nick Johnson <105008839+nickjohnsonofficial@users.noreply.github.com>
2025-03-19 11:53:27 -0400

    Update NEW_RNAseq_project.sh
    Made to run the correct program

--- NEW_RNAseq_project.sh
+++ NEW_RNAseq_project.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir  -p RNAseq/0_zips RNAseq/1_raw_data RNAseq/2_aligned RNAseq/3_counts RNAseq/4_results RNAseq/5_logs
 cp -r ~/Genome_Indices/GRCm39/ ~/RNAseq/6_indices
-bash NEW_RNAseq_project.sh
+bash RNAseq_analysis_all.sh
