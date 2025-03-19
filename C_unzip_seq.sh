274361b00584c929b99bea9d24e0d7a552aa897c
Nick Johnson <105008839+nickjohnsonofficial@users.noreply.github.com>
2025-03-19 14:14:27 -0400

    Update C_unzip_seq.sh
    fixed directories

--- C_unzip_seq.sh
+++ C_unzip_seq.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Unpacking zips"
-cd ~/RNAseq/0_zips
+cd ${rnaseq_dir}/0_zips
 fq=$1
 gunzip $fq
 base=`basename $fq .fastq.gz`
-mv ${base}.fastq ~/RNAseq/1_raw_data
+mv ${base}.fastq ${rnaseq_dir}/1_raw_data
 #for f in *.gz ; do gunzip -c "$f" > ~/RNAseq/1_raw_data/"${f%.*}" ; done
